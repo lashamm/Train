0fa33fac5473894cb751e203017bcc284096b98c
lasha <maisuradze.llasha@gmail.com>
2025-05-24 14:31:19 +0400

    still error

--- delete.js
+++ delete.js
@@ -1,19 +1,15 @@
-
-
-// fetch(`https://railway.stepprojects.ge/api/tickets/cancel/${ticketId}`, {
-//                     method: "DELETE",
-//                     headers: {
-//                         'Content-Type': 'application/json'
-//                     },
-//                 })
-
     let ticketIdSummon = JSON.parse(localStorage.getItem('tktSrtg'));
     let deleteBtn = document.querySelector('.btn')
 
     console.log(ticketIdSummon[0].people[0].seatId)
     let ticketId = ticketIdSummon[0].people[0].seatId
 
+    console.log(ticketIdSummon)
+
         deleteBtn.addEventListener('click', function(){
+             let ticketIdSummon = JSON.parse(localStorage.getItem('tktSrtg'));
+             let ticketId = ticketIdSummon[0].people[0].seatId
+             console.log(ticketId)
             fetch(`https://railway.stepprojects.ge/api/tickets/cancel/${ticketId}`, {
                     method: "DELETE",
                 })
